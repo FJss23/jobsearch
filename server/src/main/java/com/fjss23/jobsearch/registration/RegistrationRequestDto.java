63880c52720c26a23084cd425775e67b5f27546d
FJss23 <uo252473@uniovi.es>
2023-02-16 14:39:13 +0100

    feat(server/registration): add validation annotations for request dto

--- server/src/main/java/com/fjss23/jobsearch/registration/RegistrationRequestDto.java
+++ server/src/main/java/com/fjss23/jobsearch/registration/RegistrationRequestDto.java
@@ -1,9 +1,30 @@
 package com.fjss23.jobsearch.registration;
 
+import com.fjss23.jobsearch.validation.FieldsValueMatch;
+import jakarta.validation.constraints.*;
+
+@FieldsValueMatch.List(
+    {
+        @FieldsValueMatch(
+            field = "password",
+            fieldMatch = "repeatPassword",
+            message = "Passwords do not match"
+        ),
+    }
+)
 public record RegistrationRequestDto(
-        String firstName,
-        String lastName,
-        String email,
-        String password,
-        String repeatPassword
-) { }
+    @NotEmpty(message = "{error.not-empty.firstName}")
+    @Size(min = 2, max = 60, message = "{error.size.firstName}")
+    String firstName,
+    @NotEmpty(message = "{error.not-empty.lastName}")
+    @Size(min = 2, max = 60, message = "{error.size.lastName}")
+    String lastName,
+    @NotEmpty(message = "{error.not-empty.email}")
+    @Email(message = "{error.bad-format.email}")
+    String email,
+    @NotEmpty(message = "{error.not-empty.password}")
+    @Size(min = 6, max = 40, message = "{error.size.password}")
+    String password,
+    @NotEmpty(message = "{error.not-empty.repeatPassword}")
+    String repeatPassword
+) {}

c6269acc65edc5c7eabb46698a148b5cfb4d35da
FJss23 <uo252473@uniovi.es>
2023-02-18 13:28:31 +0100

    feat(server): wip implement the db queries for token and user repositories

--- server/src/main/java/com/fjss23/jobsearch/domain/registration/token/ConfirmationTokenRepository.java
+++ server/src/main/java/com/fjss23/jobsearch/domain/registration/token/ConfirmationTokenRepository.java
@@ -1,19 +1,79 @@
 package com.fjss23.jobsearch.domain.registration.token;
 
+import com.fjss23.jobsearch.domain.user.AppUser;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ConfirmationTokenRepository {
 
-    public void save(ConfirmationToken token) {}
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    private BeanPropertyRowMapper<ConfirmationToken> confirmationTokenRowMapper;
+
+    public ConfirmationTokenRepository(
+        NamedParameterJdbcTemplate jdbcTemplate
+    ) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.confirmationTokenRowMapper =
+            new BeanPropertyRowMapper<>(ConfirmationToken.class);
+    }
+
+    public void save(ConfirmationToken token) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        String sql =
+            """
+            INSERT INTO jobsearch.confirmation_token(
+                token,
+                expires_at,
+                appuser_email)
+            VALUES(
+                :token,
+                :expires_at,
+                :appuser_email);
+            """;
+        params.addValue("token", token.getToken());
+        params.addValue("expires_at", token.getExpiresAt());
+        params.addValue("appuser_email", token.getAppUserEmail());
+        jdbcTemplate.update(sql, params);
+    }
 
     public int updateConfirmedAt(String token, LocalDateTime now) {
-        return 1;
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        String sql =
+            """
+            UPDATE jobsearch.confirmation_token
+            SET confirmed_at = :confirmed_at
+            WHERE token = :token;
+            """;
+        params.addValue(":confirmed_at", now);
+        params.addValue(":token", token);
+        return jdbcTemplate.update(sql, params);
     }
 
     public Optional<ConfirmationToken> findByToken(String token) {
-        return null;
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        String sql =
+            """
+            SELECT
+                confirmation_token_id as id,
+                token,
+                expires_at,
+                confirmed_at,
+                appuser_email,
+                created_at
+            FROM jobsearch.confirmation_token
+            WHERE token = :token;
+            """;
+        params.addValue("token", token);
+        var confirmationTokens = jdbcTemplate.queryForObject(
+            sql,
+            params,
+            confirmationTokenRowMapper
+        );
+        return Optional.of(confirmationTokens);
     }
 }
--- server/src/main/java/com/fjss23/jobsearch/domain/user/AppUserRepository.java
+++ server/src/main/java/com/fjss23/jobsearch/domain/user/AppUserRepository.java
@@ -1,13 +1,28 @@
 package com.fjss23.jobsearch.domain.user;
 
 import java.util.Optional;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AppUserRepository {
 
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final BeanPropertyRowMapper<AppUser> appUserRowMapper;
+
+    public AppUserRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.appUserRowMapper = new BeanPropertyRowMapper<>(AppUser.class);
+    }
+
     Optional<AppUser> findByEmail(String email) {
-        String sql = """
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        String sql =
+            """
             SELECT
                 appuser_id as id,
                 first_name,
@@ -20,16 +35,60 @@ public class AppUserRepository {
                 logged_at,
                 company_id
             FROM jobsearch.appuser
-            WHERE email = $1
+            WHERE email = :email;
             """;
-        return null;
+        params.addValue("email", email);
+        try {
+            var appUsers = jdbcTemplate.queryForObject(sql, params, appUserRowMapper);
+            return Optional.of(appUsers);
+        } catch(EmptyResultDataAccessException ex) {
+            return Optional.empty();
+        }
     }
 
-    void save(AppUser user) {
-
+    void save(AppUser appUser) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        String sql =
+            """
+            INSERT INTO jobsearch.appuser(
+                first_name,
+                last_name,
+                email,
+                password,
+                role,
+                locked,
+                enabled,
+                created_by)
+            VALUES(
+                :first_name,
+                :last_name,
+                :email,
+                :password,
+                :role,
+                :locked,
+                :enabled,
+                :created_by);
+            """;
+        params.addValue("first_name", appUser.getFirstName());
+        params.addValue("last_name", appUser.getLastName());
+        params.addValue("email", appUser.getEmail());
+        params.addValue("password", appUser.getPassword());
+        params.addValue("role", appUser.getUserRole().toString());
+        params.addValue("locked", appUser.getLocked());
+        params.addValue("enabled", appUser.isEnabled());
+        params.addValue("created_by", appUser.getEmail());
+        jdbcTemplate.update(sql, params);
     }
 
     public int enableAppUser(String email) {
-        return 1;
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        String sql =
+            """
+            UPDATE jobsearch.appuser
+            SET enabled = :enabled
+            WHERE email = :email;
+            """;
+        params.addValue("email", email);
+        return jdbcTemplate.update(sql, params);
     }
 }

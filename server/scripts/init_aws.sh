03adcaa7fed2126d0ffbc6c066d7322d78dad585
FJss23 <uo252473@uniovi.es>
2023-02-18 13:27:33 +0100

    feat(server/localstack): adding a script for initializing aws

--- server/scripts/init_aws.sh
+++ server/scripts/init_aws.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 aws --endpoint-url=http://localhost:4566 s3 mb s3://jobsearch_bucket
+
+aws ses verirify-email-identity --email-address noreply@jobsearch.com --endpoint-url=http://localhost:4566

a87a2fc3a9eb0a2ccb0ce27ab951ac9bddd997aa
FJss23 <uo252473@uniovi.es>
2023-02-20 01:03:05 +0100

    feat(server/nvim): first steps configuring ses

--- server/scripts/init_aws.sh
+++ server/scripts/init_aws.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+# Remeber to make the file executable... chmod +x ./init_aws.sh
 
-aws --endpoint-url=http://localhost:4566 s3 mb s3://jobsearch_bucket
+# ----------------------- SES -----------------------
+
+# Step 1: Verify the email address
+aws ses verify-email-identity --email-address noreply@jobsearch.com \
+    --region eu-west-3 \
+    --endpoint-url http://localhost:4566
+
+# Step 2: Check if the email was correctly verified
+aws ses list-identities \
+    --region eu-west-3 \
+    --endpoint-url http://localhost:4566
+
+# ----------------------- S3 -----------------------
+# Step 1: Create the bucket
+# aws --endpoint-url=http://localhost:4566 s3 mb s3://jobsearch_bucket
+#
+# Step 2: Confirm if it was created
+# aws --endpoint-url=http://localhost:4566 s3 ls

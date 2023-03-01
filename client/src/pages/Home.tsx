f248d840b21f20eafc0f4e674e034ac579aada2d
FJss23 <uo252473@uniovi.es>
2023-03-01 17:09:45 +0100

    feat(client): basic home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import LatestsJobs from "../components/jobs/LatestsJobs";
+import SearchBar from "../components/jobs/SearchBar";
+import PopularTechnologies from "../components/PopularTechnologies";
+
 function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      <h1>Job Search</h1>
+      <SearchBar />
+      <LatestsJobs />
+      <PopularTechnologies />
     </>
   );
 }

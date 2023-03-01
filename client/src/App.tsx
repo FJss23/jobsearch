6eaa1266d219e978cad1c49ecade00f0b4eab212
FJss23 <uo252473@uniovi.es>
2023-03-01 17:14:57 +0100

    feat(client): putting together all the new pages in the router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,48 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ChangePasswordPage from "./pages/ChangePassword";
+import CompanyDetailPage from "./pages/CompanyDetail";
 import ErrorPage from "./pages/Error";
+import ForgotPasswordPage from "./pages/ForgotPassword";
 import HomePage from "./pages/Home";
-import RootLayout  from "./pages/Root";
+import JobDetailsPage from "./pages/JobDetails";
+import JobsPage from "./pages/Jobs";
+import JobsRoot from "./pages/JobsRoot";
+import LoginPage from "./pages/Login";
+import RegistrationPage from "./pages/Registration";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <HomePage /> }
-    ]
-  }
+      { index: true, element: <HomePage /> },
+      { path: "/registration", element: <RegistrationPage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/forgot-password", element: <ForgotPasswordPage /> },
+      { path: "/change-password", element: <ChangePasswordPage /> },
+      {
+        path: "/jobs",
+        element: <JobsRoot />,
+        children: [
+          { index: true, element: <JobsPage /> },
+          {
+            path: ":companyName",
+            element: <CompanyDetailPage />,
+          },
+          {
+            path: ":companyName/:jobTitle/:jobId",
+            element: <JobDetailsPage />,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;

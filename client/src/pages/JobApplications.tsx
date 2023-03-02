af579a2e322d831184ddb933634bd13e01ad3b92
FJss23 <uo252473@uniovi.es>
2023-03-02 11:20:12 +0100

    feat(client): define the skeleton of jobs that you have applied

---
+++ client/src/pages/JobApplications.tsx
@@ -0,0 +1,14 @@
+const JobApplicationsPage = () => {
+  return (
+    <>
+      <h1>Job Applications</h1>
+      <ul>
+        <li>Full stack</li>
+        <li>Backend 1</li>
+        <li>Backend 2</li>
+      </ul>
+    </>
+  )
+}
+
+export default JobApplicationsPage;
